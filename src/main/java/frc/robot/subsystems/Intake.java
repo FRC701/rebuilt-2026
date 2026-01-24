a3ed99869ee405a8eee3e8782134411a744471ca
Daija Canegan <daijac@vandenrobotics.com>
2026-01-24 09:59:04 -0800

    feat: Created Intake Motors and State

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -3,13 +3,42 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
+import frc.robot.Constants; 
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
   /** Creates a new Intake. */
-  public Intake() {}
+  private TalonFX m_IntakeMotor1; 
+  private TalonFX m_IntakeMotor2; 
+  private static IntakeState intakeState;
 
+  public Intake() {
+    m_IntakeMotor1 = new TalonFX(Constants.IntakeConstants.kIntakeMotor1);
+    m_IntakeMotor2 = new TalonFX(Constants.IntakeConstants.kIntakeMotor2);
+  }
+ public enum IntakeState{
+  S_Rolling,
+  S_Stop,
+  S_Outake
+ }
+
+ public void RunIntakeState() {
+  switch (intakeState){
+    case: S_Rolling:
+    break;
+    case: S_Stop:
+    break;
+    case: S_Outake:
+    break;
+  }
+ }
+
+
+
+
+
+ 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
