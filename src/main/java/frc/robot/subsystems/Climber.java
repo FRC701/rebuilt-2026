16fcbb99d0592af6c7ff910ba746401189831ea7
Zophya M <zophyam@vandenrobotics.com>
2026-01-23 16:00:17 -0800

    feat: Created Climber motors and state machines

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,12 +4,41 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Climber extends SubsystemBase {
   /** Creates a new Climber. */
-  public Climber() {}
+  private TalonFX mClimberMotor1; 
+  private TalonFX mClimberMotor2; 
 
+  public static ClimberState mClimberState;
+
+  public enum ClimberState{
+    S_Default, S_top, S_Extend, S_Lock, S_Reset
+  }
+  public Climber() {
+    mClimberMotor1 = new TalonFX(Constants.ClimberConstants.kClimberMotor1);
+    mClimberMotor2 = new TalonFX(Constants.ClimberConstants.kClimberMotor2);
+  }
+  
+  public void runClimberState(){
+   switch (mClimberState){
+    case S_Default:
+    break;
+    case S_top:
+    break;
+    case S_Extend:
+    break;
+    case S_Lock:
+    break;
+    case S_Reset:
+    break;
+    }
+  }
+ 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
