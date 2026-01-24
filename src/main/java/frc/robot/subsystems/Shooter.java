355e4c31fe753a695554e7a8b66c6e7454da261e
David Stocking <davids@vandenrobotics.com>
2026-01-24 14:14:02 -0800

    feat: Add Simple PID System
    Created PID with the shooter to create a system that will hold a velocity.

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,23 +4,57 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import com.ctre.phoenix6.controls.Follower;
+import frc.robot.Constants;
 
 public class Shooter extends SubsystemBase {
 
-  private TalonFX frontMotor;
-  private TalonFX backMotor;
+  private TalonFX mFrontMotor;
+  private TalonFX mBackMotor;
 
   /** Creates a new Shooter. */
   public Shooter(int frontId, int backId) {
-    frontMotor = new TalonFX(frontId);
-    backMotor = new TalonFX(backId);
-    backMotor.setControl(new Follower(frontId, MotorAlignmentValue.Opposed));
+    // Configs that use the PID values to help with motor speed
+    var Slot0Configs = new Slot0Configs();
+    Slot0Configs.kP = Constants.ShooterConstants.kP;
+    Slot0Configs.kI = Constants.ShooterConstants.kI;
+    Slot0Configs.kD = Constants.ShooterConstants.kD;
+    Slot0Configs.kV = Constants.ShooterConstants.kV;
+    Slot0Configs.kA = Constants.ShooterConstants.kA;
+
+    // Identifying of the motors and making the front one the leader
+    mFrontMotor = new TalonFX(frontId);
+    mBackMotor = new TalonFX(backId);
+    mBackMotor.setControl(new Follower(mFrontMotor.getDeviceID(), MotorAlignmentValue.Opposed));
+
+    // Applying the configs to the motors
+    mFrontMotor.getConfigurator().apply(Slot0Configs);
+    mBackMotor.getConfigurator().apply(Slot0Configs);
+  }
+
+  // Uses PID to arrive at our shooting speed
+  public void shooting() {
+    // voltSpeed = desired amount of rotations per second
+    VelocityVoltage voltSpeed =
+        new VelocityVoltage(Constants.ShooterConstants.shootRev).withSlot(0);
+    mFrontMotor.setControl(voltSpeed);
+  }
+
+  // Uses PID to arrive at our passing speed
+  public void passing() {
+    // voltSpeed = desired amount of rotations per second
+    VelocityVoltage voltSpeed = new VelocityVoltage(Constants.ShooterConstants.passRev).withSlot(0);
+    mFrontMotor.setControl(voltSpeed);
+  }
+
+  // Sets the speed to 0 by creating a VelocityVotage object with 0 velocity
+  public void stopping() {
+    mFrontMotor.setControl(new VelocityVoltage(0).withSlot(0));
   }
 
   @Override
