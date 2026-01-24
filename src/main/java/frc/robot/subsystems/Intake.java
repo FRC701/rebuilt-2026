4fd8f489ceb4b7f2e1ccc5aca5700e60692a453a
Zophya M <zophyam@vandenrobotics.com>
2026-01-24 10:22:22 -0800

    fix: SpotlessApply

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -3,53 +3,48 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-import frc.robot.Constants; 
-import com.ctre.phoenix6.hardware.TalonFX; 
+
+import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
   /** Creates a new Intake. */
-  private TalonFX m_IntakeMotor1; 
-  private TalonFX m_IntakeMotor2; 
+  private TalonFX m_IntakeMotor1;
+
+  private TalonFX m_IntakeMotor2;
   private static IntakeState intakeState;
 
   public Intake() {
     m_IntakeMotor1 = new TalonFX(Constants.IntakeConstants.kIntakeMotor1);
     m_IntakeMotor2 = new TalonFX(Constants.IntakeConstants.kIntakeMotor2);
   }
- public enum IntakeState{
-  S_Rolling,
-  S_Stop,
-  S_Outtake
- }
 
- public void RunIntakeState() {
-  switch (intakeState){
-    case: S_Rolling:
-    SpinMotor();
-    break;
-    case: S_Stop:
-    StopMotor();
-    break;
-    case: S_Outtake:
-    Outtake();
-    break;
+  public enum IntakeState {
+    S_Rolling,
+    S_Stop,
+    S_Outtake
   }
- }
 
-public void SpinMotor(){ 
+  public void RunIntakeState() {
+    switch (intakeState) {
+      case S_Rolling:
+        SpinMotor();
+        break;
+      case S_Stop:
+        StopMotor();
+        break;
+      case S_Outtake:
+        Outtake();
+        break;
+    }
+  }
 
-}
+  public void SpinMotor() {}
 
-public void StopMotor(){
+  public void StopMotor() {}
 
-}
-
-public void Outtake(){
-
-}
-
-
+  public void Outtake() {}
 
   @Override
   public void periodic() {
