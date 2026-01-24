0c90847e4c0a84452f92a0a76683f9424492c458
Daija Canegan <daijac@vandenrobotics.com>
2026-01-24 09:59:04 -0800

    feat: Created Intake Motors and State

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,6 +22,11 @@ public final class Constants {
     public static final int kArmMotor2 = 2;
   }
 
+  public static class IntakeConstants{
+    public static final int kIntakeMotor1 = 3;
+    public static final int kIntakeMotor2 = 4;
+  }
+
   // Shooter Motor Ids = 40s
   public static class ShooterConstants {
     // Facing from the front of the shooters (For Left and Right)
