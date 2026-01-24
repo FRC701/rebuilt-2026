d6cda2bf422b9fb11c6ebd3363ff1672783fa8a2
JennaF <jennaf@vandenrobotics.com>
2026-01-24 10:29:56 -0800

    feat: Added feeder motor and methods
    Added one motor and two methods

--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -4,29 +4,39 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Feeder extends SubsystemBase {
   /** Creates a new Feeder. */
+  private TalonFX FeederMotor;
+
   public static FeederState mFeederState;
 
   public Feeder() {
+    FeederMotor = new TalonFX();
     mFeederState = FeederState.S_Off;
   }
 
   public void runFeederState() {
     switch (mFeederState) {
       case S_On:
-        break;
+
+      break;
       case S_Off:
-        break;
+
+      break;
     }
   }
 
   public enum FeederState {
-    S_On,
-    S_Off
+    S_On, S_Off
   }
+
+  public void spinFeederMotor() {}
+
+  public void stopFeederMotor() {}
 
   @Override
   public void periodic() {
