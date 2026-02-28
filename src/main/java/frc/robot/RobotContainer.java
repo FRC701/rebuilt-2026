da0ad18e6930b3e7c6da7cc4fa196151b1a9ffe5
Kyle K <kylek@vandenrobotics.com>
2026-02-27 16:17:08 -0800

    feat: Added Named Commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import static edu.wpi.first.units.Units.*;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -94,6 +95,9 @@ public class RobotContainer {
     // Configure the trigger bindings
     m_Climber = new Climber();
     configureBindings();
+
+    NamedCommands.registerCommand("ShootLeft", new ShootingCommand(m_LeftShooter));
+    NamedCommands.registerCommand("ShootRight", new ShootingCommand(m_RightShooter));
   }
 
   /**
