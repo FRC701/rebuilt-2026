cbe08a8eb36da7ef2e6418ef159786f3271e1d8c
John Franco Saraceno <OneGneissGuy@users.noreply.github.com>
2026-03-29 20:20:54 -0700

    feat:add pose type that advantage scope expects

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -20,8 +20,8 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.DoubleArrayPublisher;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -61,7 +61,10 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
   private final VisionSubsystem m_visionSubsystem = new VisionSubsystem();
 
   /* === Field visualization === */
-  private final Field2d m_field = new Field2d();
+  private final DoubleArrayPublisher m_posePublisher =
+      NetworkTableInstance.getDefault()
+          .getDoubleArrayTopic("/Visualization/RobotPose")
+          .publish();
 
   private final SysIdRoutine m_sysIdRoutineTranslation =
       new SysIdRoutine(
@@ -133,7 +136,6 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
   }
 
   private void configureShared() {
-    SmartDashboard.putData("Field", m_field);
     if (Utils.isSimulation()) startSimThread();
   }
 
@@ -208,7 +210,9 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
     }
 
     Pose2d currentPose = getState().Pose;
-    m_field.setRobotPose(currentPose);
+    m_posePublisher.set(new double[] {
+        currentPose.getX(), currentPose.getY(), currentPose.getRotation().getRadians()
+    });
 
     if (Utils.isSimulation()) {
       m_visionSubsystem.setSimRobotPose(currentPose);
